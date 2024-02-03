aa2bf00394757b75d1039ef6efb9e045c5d4deef
sammie-k2 <132306427+sammie-k2@users.noreply.github.com>
2024-02-03 10:39:40 +0000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,7 +7,7 @@ from colorma import fore
 
 max_time = 0
 
-user_id = input("[?] UserID >>> ")
+user_id = input("7315141989866948128>>> ")
 
 while True:
   url = "https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/user/follower/list/?user_id={}&max_time={}&count=200&offset=0&source_type=2&address_book_access=2&gps_access=2&retry_type=no_retry&mcc_mnc=&app_language=en&language=en&region=US&sys_region=US&carrier_region=&carrier_region_v2=&build_number=10.5.0&timezone_offset=3600&timezone_name=Europe%2FBerlin&is_my_cn=0&fp=&pass-region=1&pass-route=1&iid=7175650440306165509&device_id=7175650438267614490&ac=wifi&channel=googleplay&aid=1233&app_name=musical_ly&version_code=100500&version_name=10.5.0&device_platform=android&ab_version=10.5.0&ssmix=a&device_type=Mi+9T&device_brand=Xiaomi&os_api=30&os_version=11&openudid=456f915916cf7837&manifest_version_code=2019032036&resolution=1080*2268&dpi=440&update_version_code=2019032036&_rticket=1672459303275&ts=1672459302&as=a1iosdfgh&cp=androide1".format(user_id, max_time)
